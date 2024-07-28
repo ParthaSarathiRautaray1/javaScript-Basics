9ce875194c5bd0b3d6230f63b493e77eea096894
ParthaSarathiRautaray1 <parthasarathirautaray176@gmail.com>
2024-07-28 09:27:04 +0530

    ArrayMethod

---
+++ 01_basics/arrayinjs.js
@@ -0,0 +1,16 @@
+const myArr = [0,1,2,3,4,5]
+const myHeros =["Batman","spiderman"]
+
+// myArr.push(9)
+// console.log(myArr)
+// myArr.pop()
+// console.log(myArr)
+
+const myNewArr = myArr.slice(1,3)
+console.log(myNewArr)
+console.log(myArr)
+
+
+const myNewArr2 = myArr.splice(1,3)
+console.log(myNewArr2)
+console.log(myArr)
